a1b4820c2e19d65cc05ca9718d216bf9a9f46ae6
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2024-01-12 15:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  //your JS code heree
+  let i = num+1;
+	while(!checkPalindrom(i)){
+	    i++;
+	}
+	return i;
+}
+
+function checkPalindrom(num) {
+	let newStr = String(num).split("").reverse().join("");
+	if(num==newStr){
+		return true;
+	}
+	return false;
 }
 
 const input = prompt("Enter a palindrome number");
